c859929f0b9fe584b20b45657543add5a3e10294
Jashleyz04 <riverajohn311@gmail.com>
2026-07-22 23:35:43 +0800

    input file contents

---
+++ stack/Data/Logs/Person/person.js
@@ -0,0 +1,9 @@
+const person = {
+    firstName: "Jose John",
+    lastName: "Rivera",
+    age: 21,
+    course: "BS Information Technology",
+  };
+  
+  console.log("Person Information");
+  console.log(person);
---
+++ stack/Data/Logs/logger.js
@@ -0,0 +1,3 @@
+console.log("This is a log message.");
+console.info("This is an info message.");
+console.warn("This is a warning message.");
